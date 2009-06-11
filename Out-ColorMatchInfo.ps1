e4dfea3332018932fb69ecfff2c92db46eee5dcc
David Mohundro <drmohundro@gmail.com>
2009-06-11 08:18:04 -0500

    Changed the match display to be closer to ack.

--- Out-ColorMatchInfo.ps1
+++ Out-ColorMatchInfo.ps1
@@ -1,15 +1,23 @@
 <#
 .Synopsis
-	Highlights MatchInfo objects similar to the output from grep.
+	Highlights MatchInfo objects similar to the output from ack.
 .Description
-	Highlights MatchInfo objects similar to the output from grep.
+	Highlights MatchInfo objects similar to the output from ack.
 #>
+#requires -version 2
 param ( 
 	[Parameter(Mandatory=$true, ValueFromPipeline=$true)] 
-	[Microsoft.PowerShell.Commands.MatchInfo] $match
+	[Microsoft.PowerShell.Commands.MatchInfo] 
+	$match,
+
+	[switch]
+	$onlyShowMatches
 )
 
-begin {}
+begin {
+	$script:priorPath = ''
+	$script:hasContext = $false
+}
 process { 
 	function Get-RelativePath([string] $path) {
 		$path = $path.Replace($pwd.Path, '')
@@ -19,33 +27,54 @@ process {
 		$path
 	}
 
-	$indent = (Get-RelativePath $match.Path).Length + $match.LineNumber.ToString().Length + 2
-
-	if ($match.Context.DisplayPreContext -ne $null) {
-		Write-Host ''.PadLeft($indent) -nonewline
-		$match.Context.DisplayPreContext
+	function Write-PathOrSeparator($match) {
+		if ($script:priorPath -ne $match.Path) {
+			''
+			Write-Host (Get-RelativePath $match.Path) -foregroundColor Green
+			$script:priorPath = $match.Path
+		}
+		else {
+			if ($script:hasContext) { 
+				'--' 
+			}
+		}
 	}
 
-	Write-Host (Get-RelativePath $match.Path) -foregroundColor DarkMagenta -nonewline
-	Write-Host ':' -foregroundColor Cyan -nonewline
-	Write-Host $match.LineNumber -foregroundColor DarkYellow -nonewline
-	Write-Host ':' -foregroundColor Cyan -nonewline
+	function Write-HighlightedMatch($match) {
+		if (-not $onlyShowMatches) {
+			Write-Host "$($match.LineNumber):" -nonewline
+		}
+		$index = 0
+		foreach ($m in $match.Matches) {
+			Write-Host $match.Line.SubString($index, $m.Index - $index) -nonewline
+			Write-Host $m.Value -ForegroundColor Black -BackgroundColor Yellow -nonewline
+			$index = $m.Index + $m.Length
+		}
+		if ($index -lt $match.Line.Length) {
+			Write-Host $match.Line.SubString($index) -nonewline
+		}
+		Write-Host ''
+	}
+	
+	function Write-ContextLines($context, $contextLines) {
+		if ($context.length -eq $null) {return}
 
-	$index = 0
-	foreach ($m in $match.Matches) {
-		Write-Host $match.Line.SubString($index, $m.Index - $index) -nonewline
-		Write-Host $m.Value -ForegroundColor Red -nonewline
-		$index = $m.Index + $m.Length
+		$script:hasContext = $true
+		for ($i = 0; $i -lt $context.length; $i++) {
+			"$($contextLines[$i])- $($context[$i])"
+		}
 	}
-	if ($index -lt $match.Line.Length) {
-		Write-Host $match.Line.SubString($index) -nonewline
+	
+	if (-not $onlyShowMatches) {
+		Write-PathOrSeparator $match
 	}
-	''
 
-	if ($match.Context.DisplayPostContext -ne $null) {
-		Write-Host ''.PadLeft($indent) -nonewline
-		$match.Context.DisplayPostContext
-		'-------------------------------'
-	}
+	$lines = ($match.LineNumber - $match.Context.DisplayPreContext.Length)..($match.LineNumber - 1)
+	Write-ContextLines $match.Context.DisplayPreContext $lines
+
+	Write-HighlightedMatch $match
+
+	$lines = ($match.LineNumber + 1)..($match.LineNumber + $match.Context.DisplayPostContext.Length)
+	Write-ContextLines $match.Context.DisplayPostContext $lines
 }
 end {}
