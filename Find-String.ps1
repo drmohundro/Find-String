81b57a83a4df27b0e78b01b5ee0eed4a20eeb1f6
David Mohundro <drmohundro@gmail.com>
2009-06-11 08:10:35 -0500

    Refactored highlighting of match out of script
    The highlighting of matches is now handled by Out-HighlightedMatchInfo.

--- Find-String.ps1
+++ Find-String.ps1
@@ -16,34 +16,6 @@ if ((-not $caseSensitive) -and (-not $pattern.Options -match "IgnoreCase")) {
 	$pattern = New-Object regex $pattern.ToString(),@($pattern.Options,"IgnoreCase")
 }
 
-function Write-HighlightedMatch {
-	process { 
-		Write-Host (Get-RelativePath $_.Path) -foregroundColor DarkMagenta -nonewline
-		Write-Host ':' -foregroundColor Cyan -nonewline
-		Write-Host $_.LineNumber -foregroundColor DarkYellow -nonewline
-		Write-Host ':' -foregroundColor Cyan -nonewline
-	
-		$index = 0
-		foreach ($match in $_.Matches) {
-			Write-Host $_.Line.SubString($index, $match.Index - $index) -nonewline
-			Write-Host $match.Value -ForegroundColor Red -nonewline
-			$index = $match.Index + $match.Length
-		}
-		if ($index -lt $_.Line.Length) {
-			Write-Host $_.Line.SubString($index) -nonewline
-		}
-		''
-	}
-}
-
-function Get-RelativePath([string] $path) {
-    $path = $path.Replace($pwd.Path, '')
-    if ($path.StartsWith('\') -and (-not $path.StartsWith('\\'))) { 
-        $path = $path.Substring(1) 
-    }
-    $path
-}
-
 Get-ChildItem -recurse:$recurse -filter:$filter |
 	Select-String -caseSensitive:$caseSensitive -pattern:$pattern -AllMatches | 
-	Write-HighlightedMatch
+	Out-HighlightMatchInfo
---
+++ Out-HighlightMatchInfo.ps1
@@ -0,0 +1,38 @@
+<#
+.Synopsis
+	Highlights MatchInfo objects similar to the output from grep.
+.Description
+	Highlights MatchInfo objects similar to the output from grep.
+#>
+param ( 
+	[Parameter(Mandatory=$true, ValueFromPipeline=$true)] 
+	[Microsoft.PowerShell.Commands.MatchInfo] $match
+)
+
+begin {}
+process { 
+	function Get-RelativePath([string] $path) {
+		$path = $path.Replace($pwd.Path, '')
+		if ($path.StartsWith('\') -and (-not $path.StartsWith('\\'))) { 
+			$path = $path.Substring(1) 
+		}
+		$path
+	}
+
+	Write-Host (Get-RelativePath $match.Path) -foregroundColor DarkMagenta -nonewline
+	Write-Host ':' -foregroundColor Cyan -nonewline
+	Write-Host $match.LineNumber -foregroundColor DarkYellow -nonewline
+	Write-Host ':' -foregroundColor Cyan -nonewline
+
+	$index = 0
+	foreach ($m in $match.Matches) {
+		Write-Host $match.Line.SubString($index, $m.Index - $index) -nonewline
+		Write-Host $m.Value -ForegroundColor Red -nonewline
+		$index = $m.Index + $m.Length
+	}
+	if ($index -lt $match.Line.Length) {
+		Write-Host $match.Line.SubString($index) -nonewline
+	}
+	''
+}
+end {}
