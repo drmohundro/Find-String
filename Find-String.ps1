1d98b23a9cbf1f4a8c5b8bd7233fd3b91724e196
David Mohundro <drmohundro@gmail.com>
2009-06-11 08:07:13 -0500

    Tweaked display and bumped to PoSH v2
    I added the display of the FileName and LineNumber together. This
    change was to get the display closer to that of grep.

--- Find-String.ps1
+++ Find-String.ps1
@@ -1,45 +1,41 @@
-# Find-String.ps1
-#  Wrapper around dir | select-string which will highlight the pattern in the results
-#  http://weblogs.asp.net/whaggard/archive/2007/03/23/powershell-script-to-find-strings-and-highlight-them-in-the-output.aspx
+<#
+.Synopsis
+	Searches text files by pattern and displays the results.
+.Description
+	Searches text files by pattern and displays the results.
+#>
 param ( 
-	[string] $pattern = "",
+	[Parameter(Mandatory=$true)] 
+	[regex] $pattern,
 	[string] $filter = "*.*",
 	[switch] $recurse = $true,
 	[switch] $caseSensitive = $false
 )
 
-if ($pattern -eq $null -or $pattern -eq "") { Write-Error "Please provide a search pattern!" ; return }
+if ((-not $caseSensitive) -and (-not $pattern.Options -match "IgnoreCase")) {
+	$pattern = New-Object regex $pattern.ToString(),@($pattern.Options,"IgnoreCase")
+}
 
-$regexPattern = $pattern
-if ($caseSensitive -eq $false) { $regexPattern = "(?i)$regexPattern" }
-$regex = New-Object System.Text.RegularExpressions.Regex $regexPattern
-
-# Write the line with the pattern highlighted in red
-function Write-HostAndHighlightPattern([string]$inputText)
-{
-	$index = 0
-	while($index -lt $inputText.Length)
-	{
-		$match = $regex.Match($inputText, $index)
-		if ($match.Success -and $match.Length -gt 0)
-		{
-			Write-Host $inputText.SubString($index, $match.Index - $index) -nonewline
-			Write-Host $match.Value.ToString() -ForegroundColor Red -nonewline
+function Write-HighlightedMatch {
+	process { 
+		Write-Host $_.Filename -foregroundColor DarkMagenta -nonewline
+		Write-Host ':' -foregroundColor Cyan -nonewline
+		Write-Host $_.LineNumber -foregroundColor DarkYellow -nonewline
+		Write-Host ':' -foregroundColor Cyan -nonewline
+	
+		$index = 0
+		foreach ($match in $_.Matches) {
+			Write-Host $_.Line.SubString($index, $match.Index - $index) -nonewline
+			Write-Host $match.Value -ForegroundColor Red -nonewline
 			$index = $match.Index + $match.Length
 		}
-		else
-		{
-			Write-Host $inputText.SubString($index) -nonewline
-			$index = $inputText.Length
+		if ($index -lt $_.Line.Length) {
+			Write-Host $_.Line.SubString($index) -nonewline
 		}
+		''
 	}
 }
 
-# Do the actual find in the files
-Get-ChildItem -recurse:$recurse -filter:$filter | 
-	Select-String -caseSensitive:$caseSensitive -pattern:$pattern |pattern
-		foreach {
-			Write-Host "$($_.RelativePath($pwd.Path))($($_.LineNumber)): " -nonewline
-			Write-HostAndHighlightPattern $_.Line
-			Write-Host
-		}
+Get-ChildItem -recurse:$recurse -filter:$filter |
+	Select-String -caseSensitive:$caseSensitive -pattern:$pattern -AllMatches | 
+	Write-HighlightedMatch
